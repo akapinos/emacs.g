b5ccb164302612fe18c1dabad0191f1bb3c07a22
akapinos <art.harakiri@gmail.com>
2022-01-30 18:26:40 +0300

    Assimilate hl-todo v3.3.0-4-g42f744f

---
+++ alexk.el
@@ -0,0 +1,44 @@
+(defface hl-todo-TODO-face
+  '((t (:underline t :foreground "Dark Red" :inherit (hl-todo))))
+  "Face for highlighting TODO keyword."
+  :group 'hl-todo)
+
+(defface hl-todo-FIXME-face
+  '((t (:underline t :foreground "Dark Magenta" :inherit (hl-todo))))
+  "Face for highlighting FIXME keyword."
+  :group 'hl-todo)
+
+(defface hl-todo-DEBUG-face
+  '((t (:underline t :foreground "Dark Cyan" :inherit (hl-todo))))
+  "Face for highlighting DEBUG keyword."
+  :group 'hl-todo)
+(defface hl-todo-MAKE-face
+  '((t (:underline t :foreground "Dark Orange" :inherit (hl-todo))))
+  "Face for highlighting MAKE keyword."
+  :group 'hl-todo)
+
+(defface hl-todo-NOTE-face
+  '((t (:underline t :foreground "Dark Khaki" :inherit (hl-todo))))
+  "Face for highlighting NOTE keyword"
+  :group 'hl-todo)
+
+(defface hl-todo-DONE-face
+  '((t (:underline t :foreground "Dark Green" :inherit (hl-todo))))
+  "Face for highlighting DONE keyword."
+  :group 'hl-todo)
+
+(use-package hl-todo
+  :init (hl-todo-mode)
+  :config
+  (setq hl-todo-keyword-faces
+  '(("TODO"  . hl-todo-TODO-face)
+    ("FIXME" . hl-todo-FIXME-face)
+    ("DEBUG" . hl-todo-DEBUG-face)
+    ("MAKE"  . hl-todo-MAKE-face)
+    ("NOTE"  . hl-todo-NOTE-face)
+    ("DONE"  . hl-todo-DONE-face)))
+  :bind
+  (("C-c p" . hl-todo-previous)
+   ("C-c n" . hl-todo-next)
+   ("C-c o" . hl-todo-occur)
+   ("C-c i" . hl-todo-insert)))
